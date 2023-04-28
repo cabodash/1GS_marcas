68f9db800add8d27ac91c848c3ac9c8ee9a394be
cabodash <alejandro.cabo.guisado@gmail.com>
2023-04-28 22:50:46 +0200

    añadida practica 31

--- 3ºEvaluacion/practica31/ej3.js
+++ 3ºEvaluacion/practica31/ej3.js
@@ -1,2 +1,13 @@
-const cuadrado = document.createElement("div");
-const colors = ["red", "green", "blue", "yellow", "pink", "purple", "orange"];
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange'];
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+const appDiv = document.querySelector('.app');
+appDiv.classList.add(randomColor);
+
+if (randomColor === 'yellow' || randomColor === 'pink' || randomColor === 'purple') {
+  appDiv.classList.add('medium');
+} else if (randomColor === 'orange') {
+  appDiv.classList.add('large');
+} else {
+  appDiv.classList.add('small');
+}
+appDiv.textContent = 'Soy un cuadrado en el centro';
